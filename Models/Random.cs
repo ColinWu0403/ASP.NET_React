997762aef70aa2c6654b44fa9041de91b6f18eec
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-13 15:52:01 +0800

    Random

---
+++ Models/Random.cs
@@ -0,0 +1,545 @@
+
+Skip to main content
+Microsoft
+.NET
+
+    Downloads Community LIVE TV 
+
+Build. Test. Deploy.
+
+.NET is the free, open-source, cross-platform framework for building modern apps and powerful cloud services.
+Download
+Get started
+
+Supported on macOS, Windows, and Linux
+Build it with .NET
+
+Web
+
+Build web apps and services for macOS, Windows, Linux, and Docker.
+
+Mobile and desktop
+
+Use a single codebase to build native apps for Windows, macOS, iOS, and Android.
+
+Cloud
+
+Build scalable and resilient cloud-native apps that run on all major cloud providers.
+
+Microservices
+
+Create independently deployable microservices that run on Docker containers.
+Artificial Intelligence & ML
+Game development
+Internet of Things
+Mobile
+Desktop
+Front-end web
+Back-end APIs
+Data
+Faster response times, less compute power, better applications
+
+The Fortunes test simulates a simple web application where HTML is rendered server-side after querying a database. See TechEmpower's Round 22 results.
+Fortunes responses per second
+Three charts showing TechEmpower results based on fortunes responses per second - 33.9K for Node.js (Express), 342.5K for ASP.NET Core, and 62.6K for Go (Gin)
+What's next in .NET 9?
+
+.NET 9 elevates cloud-native and intelligent app development, and more. Preview the next major version of .NET today.
+See what's next
+.NET at Build 2024
+
+Missed Build 2024? Catch up on all the news and updates around .NET at Build this year.
+Learn more
+Dive deeper into .NET + AI
+
+Use these hand-picked resources to discover how to use .NET to build an amazing AI-infused application!
+Get started
+Free and open source
+
+NET is developed and maintained as a free and open-source project on GitHub, the home for millions of developers who want to build great things together.
+Fast and cross-platform
+
+You can write, run, and build on multiple platforms, including Windows, Linux, and macOS.
+Everything you need to start your .NET learning journey
+
+.NET offers a library of learning resources. Access videos, tutorials, code samples, and content from Microsoft Learn to help you build better.
+Explore learning portal
+
+.NET for Beginners videos
+
+Getting started with .NET development? We have you covered with our .NET for Beginners videos. Explore videos on web, mobile, desktop, C#, machine learning, containers/Docker, data access, and more.
+Browse beginner videos
+
+Microsoft Learn
+
+Discover your path to build apps with .NET on Microsoft Learn. Whether you're just starting or an experienced professional, Microsoft Learn's hands-on approach helps you arrive at your goals faster, with more confidence and at your own pace for free.
+Get started
+
+LinkedIn Learning
+
+Get an introduction to the programming skills needed for a career as a .NET software developer. Experience .NET learning courses that provide a broad perspective on core technologies leveraging .NET.
+Explore courses
+Productive
+With its large supportive ecosystem and powerful tooling, .NET is the most
+productive platform for developers.
+NuGet
+
+Build with the most secure package ecosystem trusted by millions of developers. With over 390,000 unique packages to date and 50,000 more each year.
+Browse the NuGet Gallery
+Visual Studio
+
+Develop with the best IDE for .NET developers. Packed with an array of tools and features to elevate and enhance every stage of software development.
+Meet the Visual Studio family
+Visual Studio Code
+
+Develop on Linux, macOS, or Windows to build cross-platform websites and services. Install the C# Dev Kit to get the best experience.
+Download Visual Studio Code
+Azure
+
+Create modern, scalable cloud apps on a cloud platform designed for .NET.
+Explore Azure resources and tools
+Be a part of the global .NET developer community
+
+Join the large, engaged community of developers around the globe who build with .NET. Share, showcase, and solve together. Stay connected.
+Get connected
+
+474K+
+stars on GitHub
+
+330K+
+followers on X
+
+329K+
+members on Meetup
+Who uses .NET?
+
+The world's top organizations are powered by .NET and trust Microsoft to make .NET the best choice in the industry for their mission critical software. Amazing developers like you use .NET to build incredible software for millions of companies around the world, in every industry.
+Read customer stories
+Ready to get started?
+
+Learn .NET easily with our step-by-step tutorials.
+Get started
+Follow us
+
+Dark mode
+
+            
+            
+
+            
+
+            
+
+            
+
+        
+Your Privacy Choices
+Consumer Health Privacy
+
+    Powered by .NET 8.0.6 Contact Microsoft Support Privacy & Cookies Manage cookies Terms of Use Trademarks © Microsoft 2024 
+
+
+Skip to main content
+Microsoft
+.NET
+
+    Downloads Community LIVE TV 
+
+    Home
+    Learning center
+
+Learn .NET
+
+Free tutorials, videos, courses, and more for beginner through advanced .NET developers.
+What is .NET?
+
+.NET is a free, cross-platform, open source developer platform for building many different types of applications.
+With .NET, you can use multiple languages, editors, and libraries to build for web, mobile, desktop, games, IoT, and more.
+Learn more
+What do you want to learn?
+
+Programming with C#
+
+Learn the building blocks of programming using C#, .NET's open-source, cross-platform, object-oriented programming language.
+
+Front-end web
+
+Use Blazor to build interactive web pages with HTML, CSS, and C#.
+
+Back-end web
+
+Start building powerful, versatile APIs with ASP.NET Core.
+
+Cloud and Microservices
+
+Create independently deployable microservices that run on docker containers.
+
+Mobile and desktop
+
+Use a single codebase to build native mobile apps for iOS, Android, and more.
+
+Programming with F#
+
+Write succinct, robust, and performant code.
+
+Artificial Intelligence and ML
+
+Add vision algorithms, speech processing, predictive models, and more to your apps.
+
+Game development
+
+Develop 2D and 3D games for the most popular desktops, phones, and consoles.
+How do you want to learn?
+Videos
+
+Watch the .NET for Beginners video series. Explore videos on web, mobile, desktop, C#, machine learning, containers/Docker, data access, and more.
+View videos
+Tutorials
+
+Take a step-by-step tutorial on Microsoft Learn site. There are tutorials on programming with C#, web development, and more!
+Go to tutorials
+Documentation
+
+Refer to the .NET documentation for specific guidance on product features.
+Explore documentation
+Architecture Guides
+
+Free e-books, videos, and practical advice to help you build better apps with .NET.
+See architecture guides
+Follow us
+
+Dark mode
+
+            
+            
+
+            
+
+            
+
+            
+
+        
+Your Privacy Choices
+Consumer Health Privacy
+
+    Powered by .NET 8.0.6 Contact Microsoft Support Privacy & Cookies Manage cookies Terms of Use Trademarks © Microsoft 2024 
+
+
+Skip to main content
+Take the challenge
+
+May 21 – June 21, 2024
+Learn
+
+Sign in
+.NET
+
+.NET documentation
+
+Learn to use .NET to create applications on any platform using C#, F#, and Visual Basic. Browse API reference, sample code, tutorials, and more.
+
+Download
+Download .NET
+
+Training
+Build .NET apps with C#
+
+Training
+Create your first web app
+
+Get Started
+Interactive introduction to C#
+
+Architecture
+.NET architecture docs
+
+Overview
+Azure for .NET developers
+
+Overview
+.NET Aspire
+
+What's new
+What's new in .NET
+.NET: Free. Cross platform. Open source.
+
+A developer platform for building all your apps: web, mobile, desktop, gaming, IoT, and more. Supported on Windows, Linux, and macOS.
+
+Open source .NET
+
+    .NET overview
+    .NET tutorials
+    What's new in .NET 8
+    .NET Foundation
+    .NET developer community
+    Community Toolkits
+
+.NET concepts
+
+    What is .NET?
+    .NET fundamentals
+    .NET implementations
+    .NET Framework (for Windows)
+    .NET Standard
+    .NET Q&A
+    .NET tech community forums
+    Security in .NET
+
+Develop .NET apps
+
+    Create .NET desktop apps for Windows
+    Create web apps with ASP.NET Core
+    Build cloud-native .NET apps with Orleans
+    Build cloud-native apps with .NET Aspire
+    Containerize .NET apps with Docker
+
+.NET tools and diagnostics
+
+    .NET SDK overview
+    .NET CLI overview
+    MSBuild property reference
+    Global and local tools
+    Diagnostics tools
+    Code analysis
+    Package validation
+
+Migrate and upgrade
+
+    Port from .NET Framework to .NET
+    .NET breaking changes
+    Download .NET
+
+DevOps and testing
+
+    Deploy .NET apps
+    Unit test .NET apps
+    GitHub Actions and .NET
+
+Data access in .NET
+
+    LINQ overview
+    Entity Framework Core
+    Azure Storage
+    XML data
+
+Advanced .NET programming
+
+    Async programming patterns overview
+    Threading overview
+    Parallel programming overview
+    Native interoperability
+    Garbage collection
+
+Programming languages
+
+Write your app in your favorite language
+Write .NET apps in C#, F#, or Visual Basic
+A modern, object-oriented, and type-safe language
+A simple language for succinct, robust, and performant code
+An approachable language with readable syntax
+Create your application
+
+You can choose web, mobile, desktop, gaming, IoT, and more.
+
+Web
+
+    ASP.NET Core tutorials
+    What is ASP.NET Core?
+    ASP.NET Core in Visual Studio
+    ASP.NET MVC apps in Windows containers
+    Blazor: Interactive client-side web UI with .NET
+    F# for web development
+
+Mobile
+
+    .NET Multi-platform App UI (.NET MAUI)
+    Xamarin.Forms
+    Xamarin.iOS
+    Xamarin.Android
+    Develop Xamarin apps with Azure
+
+Desktop
+
+    Universal Windows apps
+    Windows Presentation Foundation (.NET 5+)
+    Windows Presentation Foundation (.NET Framework)
+    Windows Forms (.NET 5+)
+    Windows Forms (.NET Framework)
+    .NET Multi-platform App UI (.NET MAUI)
+    Xamarin for macOS
+
+Microservices
+
+    Dapr for .NET developers
+    Cloud native .NET apps
+    Serverless apps with Azure
+    Architecture for containerized .NET apps
+    Deploy a Worker Service to Azure
+
+Cloud
+
+    Azure for .NET developers
+    .NET Aspire
+    Migrate on-premises .NET web apps or services
+    Azure services for .NET developers
+    Azure SDK for .NET
+    Deploying Azure Resources with F#
+
+AI and machine learning
+
+    AI for .NET developers
+    Build a chat app
+    Understand prompt engineering
+    Implement RAG using vector search
+    Get started with the .NET enterprise chat sample
+    Build custom AI solutions with ML.NET
+    Azure Cognitive Services
+    Azure machine learning
+    F# for Machine Leaning
+
+Game development
+
+    Game development with Visual Studio
+    Learn how to use CRYENGINE to build games with C#
+    Build games with C# using the MonoGame library
+    Learn how to use Unity to build 2D and 3D games with C#
+
+Internet of things (IoT)
+
+    .NET IoT Libraries
+    Get started in 5 minutes
+    Blink an LED
+    .NET IoT 101 video series
+
+API and language reference
+
+Search the .NET API and language reference documentation.
+.NET API reference
+
+API reference documentation for .NET
+.NET Framework API reference
+
+API reference documentation for .NET Framework
+ASP.NET Core API reference
+
+API reference documentation for ASP.NET Core
+ML.NET API reference
+
+API reference documentation for ML.NET
+.NET Platform Extensions API reference
+
+API reference documentation for .NET Platform Extensions
+C# language reference
+
+C# language reference and specification
+F# language reference
+
+F# language reference
+Visual Basic language reference
+
+Visual Basic language reference and specification
+
+Are you interested in contributing to the .NET docs? For more information, see our contributor guide.
+
+    Previous Versions
+    Blog
+    Contribute
+    Privacy
+    Terms of Use
+    Trademarks
+    © Microsoft 2024
+
+
+Skip to main content
+Microsoft
+.NET
+
+    Downloads Community LIVE TV 
+
+    Home
+    Learning center
+    Application architecture guides
+
+.NET Architecture Guides
+
+Learn how to build production-ready .NET apps with free application architecture guidance.
+Microservices architecture e-book
+
+This guide is an introduction to developing microservices-based applications and managing them using containers. It discusses architectural design and implementation approaches using .NET Core and Docker containers.
+
+Download PDF
+
+Looking for the e-book in another format?
+
+    View on the web
+
+ASP.NET Core architecture e-book
+
+This guide provides end-to-end guidance on building monolithic web applications using ASP.NET Core and Azure.
+
+Download PDF
+
+Looking for the e-book in another format?
+
+    View on the web
+
+Cloud-native e-book
+
+This free e-book defines cloud native, introduces a sample app built using cloud-native principles, and covers topics common to most cloud-native applications.
+
+Download PDF
+
+Looking for the e-book in another format?
+
+    View on the web
+
+Blazor e-book
+
+This free e-book introduces ASP.NET Web Forms developers to Blazor. It introduces Blazor concepts in parallel with analogous concepts in ASP.NET Web Forms.
+
+Download PDF
+
+Looking for the e-book in another format?
+
+    View on the web
+
+Enterprise Application Patterns Using .NET MAUI
+
+This e-book provides guidance on how to build cross-platform enterprise apps using .NET MAUI.
+
+Download PDF
+
+Looking for the e-book in another format?
+
+    View on the web
+
+Azure quick start e-book
+
+In this short guide, explore Microsoft Azure services for a variety of .NET application scenarios. Quickly learn about the services that are right for you.
+
+Download PDF
+
+Ready to get started?
+
+Learn .NET easily with our step-by-step tutorials.
+
+Follow us
+
+Dark mode
+
+            
+            
+
+            
+
+            
+
+            
+
+        
+Your Privacy Choices
+Consumer Health Privacy
+
+    Powered by .NET 8.0.6 Contact Microsoft Support Privacy & Cookies Manage cookies Terms of Use Trademarks © Microsoft 2024 
+
